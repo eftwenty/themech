7be234214a0c610197414cfc85767e5dfc8f8101
eftwenty <ftwnt20@gmail.com>
2016-03-13 22:57:19 +0200

    Added User model

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,7 @@
+class UsersController < ApplicationController
+  layout 'user_layout'
+
+  def index
+
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  resource :users do
+    get :index
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
