f484a6c9103753d239b57e3ffbeafc22028f0b87
eftwenty <ftwnt20@gmail.com>
2016-03-14 00:08:24 +0200

    wip

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,19 @@ class UsersController < ApplicationController
   layout 'user_layout'
 
   def index
+    @title = 'Users'
+  end
 
+  def new
+    @user = User.new params[:user_params]
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(
+        :first_name,
+        :last_name
+    )
   end
 end
